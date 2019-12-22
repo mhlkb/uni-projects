23ede9dc2dd8415c442410941a21e4afdc5c144b
mhlkb <57629867+mhlkb@users.noreply.github.com>
2019-12-22 13:06:00 +0100

    Add files via upload

---
+++ nearMePedia/style.js
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native'
+
+import Constants from 'expo-constants'
+
+
+
+
+
+const styles = StyleSheet.create({
+
+	container: {
+
+		paddingTop: Constants.statusBarHeight,
+
+		flex: 1,
+
+		backgroundColor: '#fff',
+
+		justifyContent: 'center',
+
+	},
+pages: {
+
+  fontSize: 25,
+
+},
+buttonPages: {
+
+  paddingRight: 50,
+
+  	flex: 1,
+
+		backgroundColor: '#fff',
+
+		justifyContent: 'center',
+},
+});
+
+
+
+export default styles
